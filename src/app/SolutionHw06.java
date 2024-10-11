36f6092c5b06ae0f70e02d74685dcde85d7f78af
InnaDudka <innadudka1@gmail.com>
2024-10-11 22:22:38 +0300

    First commit

--- src/app/SolutionHw06.java
+++ src/app/SolutionHw06.java
@@ -4,10 +4,10 @@ public class SolutionHw06 {
     public static void main(String[] args) {
         int[] source = new int[]{1, 2, 3, 4, 5, 6};
         int sum = 0;
-        for (int index =0;index < source.length; index++) {
-            sum +=source[index];
+        for (int i : source) {
+            sum += i;
             System.out.printf("%d Num is %d, sum is %d %n",
-                   source[index], source[index], sum);
+                    i, i, sum);
         }
         System.out.printf("Sum of numbers is %d", sum);
 
